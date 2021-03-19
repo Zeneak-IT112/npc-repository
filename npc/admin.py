20fb4915e5115ad3736f9c4ebd4da2e245ef86a7
Zeneak <Joshua.Zimmerschied@gmail.com>
2021-03-19 12:16:09 -0700

    Models complete

--- npc/admin.py
+++ npc/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import PersonType, Person, PlaceType, Place
+
+admin.site.register(PersonType)
+admin.site.register(Person)
+admin.site.register(PlaceType)
+admin.site.register(Place)
 
 # Register your models here.
