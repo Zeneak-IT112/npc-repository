2bb3a70dee538e46712d3a4dd77ce731256e0cee
Gabe DeGuzman <gedeguz007@gmail.com>
2021-03-19 12:53:32 -0700

    Added views and url links to base

--- npc/views.py
+++ npc/views.py
@@ -1,5 +1,16 @@
+from .models import Person, PersonType, Place, PlaceType
 from django.shortcuts import render
 
 # Create your views here.
 def index (request):
     return render(request, 'npc/index.html')
+
+def person_type(request):
+    person_type_list=PersonType.objects.all()
+    return render(request, 'npc/persontypes.html' ,{'person_type_list' : person_type_list})
+
+def place_type(request):
+    place_type_list=PlaceType.objects.all()
+    return render(request, 'npc/placetypes.html' ,{'place_type_list' : place_type_list})
+
+ 
