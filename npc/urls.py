c7a37116833b472d6816a8d0f4d62d5dcd37ccec
Zeneak <Joshua.Zimmerschied@gmail.com>
2021-03-21 11:49:25 -0700

    Added persondetails view and html, and more!

--- npc/urls.py
+++ npc/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns=[
     path('', views.index, name='index'),
-    path('person_type/', views.person_type, name='persontypes'),
-    path('place_type/', views.place_type, name='placetypes'),
-    path('person/', views.person, name='person'),
+    path('persontypes/', views.person_type, name='persontypes'),
+    path('placetypes/', views.place_type, name='placetypes'),
+    path('personlist/', views.person_list, name='personlist'),
+    path('persondetails/<int:id>', views.person_details, name='persondetails'),
 ]
